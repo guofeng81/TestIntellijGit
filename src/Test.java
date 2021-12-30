f242d5efa65de84ce58cad2786a7484c7d6f7dd0
guofeng81 <guofeng81@yahoo.com>
2021-12-30 12:17:32 -0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Test("Hello");
+    }
+
+    public static void Test(String str){
+        System.out.println(str);
+    }
+}
